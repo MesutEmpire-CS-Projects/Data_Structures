630191a7fe99ea290bbf0e66de633d91efc770c1
Mirror83 <glenochieng045@gmail.com>
2023-11-05 21:46:39 +0300

    Updated main to use group modes

--- Treap/main.py
+++ Treap/main.py
@@ -1,7 +1,7 @@
 """
 Rough driver code for a student group maker
 """
-from Treap.student_group_maker import StudentGroupMaker
+from Treap.student_group_maker import StudentGroupMaker, GroupMode
 
 
 def main():
@@ -10,20 +10,36 @@ def main():
     try:
         group_maker = StudentGroupMaker(path)
     except IOError:
-        print(f"The file {path}, cannot be opened.")
+        print(f"The file {path}, cannot be opened. Please check if name is typed correctly")
         exit(1)
     except StudentGroupMaker.InvalidFileException:
         print("The file must be a csv file")
         exit(2)
+    except StudentGroupMaker.DuplicateKeyException:
+        print("The file has duplicate registration numbers. Please correct this first.")
+        exit(3)
 
     try:
         students_per_group = int(input("Enter the number of students per group: "))
-    except ValueError:
-        # TODO: Handle this case appropriately
-        print("Please enter a valid number")
-        exit(3)
+        print("Group modes")
+        print("-" * 10)
+        print("1. Random")
+        print("2. Ascending")
+        print("3. Descending")
 
-    group_maker.make_groups(students_per_group)
+        group_mode_input = int(input("Enter group mode: "))
+        if 1 <= group_mode_input <= 3:
+            group_mode = GroupMode(group_mode_input)
+        else:
+            print("Invalid choice. Please choose 1, 2 or 3 for group mode")
+            exit(4)
+
+    except ValueError:
+        # TODO: Handle this case appropriately, probably by forcing a loop until a valid value is entered
+        print("Please enter a valid number")
+        exit(5)
+
+    group_maker.make_groups(students_per_group, group_mode)
     print(f"The file with groups is at ./{StudentGroupMaker.OUTPUT_FILE}")
 
 
